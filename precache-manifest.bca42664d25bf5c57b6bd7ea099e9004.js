46c8d415d50165c6ae1dabf1465bfc43f5e09946
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-09 20:11:04 +0300

    Updates

---
+++ precache-manifest.bca42664d25bf5c57b6bd7ea099e9004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdf8ff7784cf59582d0e940e52eb224",
+    "url": "/todo1/index.html"
+  },
+  {
+    "revision": "9705e660dffc107cdabf",
+    "url": "/todo1/static/css/main.5b5000ea.chunk.css"
+  },
+  {
+    "revision": "3c0d95dc07d5579b6667",
+    "url": "/todo1/static/js/2.b3e8e9c3.chunk.js"
+  },
+  {
+    "revision": "9705e660dffc107cdabf",
+    "url": "/todo1/static/js/main.e0ae7028.chunk.js"
+  },
+  {
+    "revision": "2778f72e201b5a3f5dbc",
+    "url": "/todo1/static/js/runtime~main.a1458fc9.js"
+  }
+]);
